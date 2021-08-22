182936993198eb4e7fd792d4efca446841e43cd2
Matthew Lau <matthew.lau78@gmail.com>
2021-08-22 21:47:29 +1000

    landing page started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { Landing } from "./pages/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route exact path="/"></Route>
+          <Route exact path="/"></Route>
+          <Route exact path="/"></Route>
+          <Route exact path="/"></Route>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
