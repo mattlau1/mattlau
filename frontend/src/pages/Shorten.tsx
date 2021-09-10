4f7a6227a807304bf172f80997d6d932b0d4bae5
Matthew Lau <matthew.lau78@gmail.com>
2021-09-11 01:38:56 +1000

    link shortening working with alerts

--- frontend/src/pages/Shorten.tsx
+++ frontend/src/pages/Shorten.tsx
@@ -1,11 +1,16 @@
+import axios, { AxiosError } from "axios";
 import React, { useEffect, useState } from "react";
 
 interface ShortenProps {}
 
-export const Shorten: React.FC<ShortenProps> = ({}) => {
+export const Shorten: React.FC<ShortenProps> = () => {
   const [alertVisible, setAlertVisible] = useState(false);
   const [buttonToggle, setButtonToggle] = useState(false);
   const [linkText, setLinkText] = useState("");
+  const [alertTitle, setAlertTitle] = useState("");
+  const [alertDesc, setAlertDesc] = useState("");
+  const [errorAlert, setErrorAlert] = useState(false);
+
   useEffect(() => {
     document.title = "Matthew Lau | Link Shortener";
   }, []);
@@ -15,18 +20,64 @@ export const Shorten: React.FC<ShortenProps> = ({}) => {
     setButtonToggle(linkText.includes(window.location.host));
   }, [linkText]);
 
+  const shortenLink = () => {
+    axios
+      .post(`${process.env.API || "http://localhost:5000"}/shorten`, { fullURL: linkText })
+      .then((res) => {
+        console.log(window.location.origin + "/" + res.data.url);
+        setLinkText(window.location.origin + "/" + res.data.url);
+      })
+      .catch((err: AxiosError) => {
+        console.log(err);
+        if (err.response!.status === 400) {
+          setInvalidLinkAlert();
+          showAlert();
+        }
+      });
+  };
+
+  const setInvalidLinkAlert = () => {
+    setAlertTitle("Invalid Link Provided");
+    setAlertDesc("The provided link is not a valid link. Please check the link and try again.");
+    setErrorAlert(true);
+  };
+
+  const setSuccessfulCopyAlert = () => {
+    setAlertTitle("Link was successfully copied to clipboard");
+    setAlertDesc("");
+    setErrorAlert(false);
+  };
+
+  const showAlert = () => {
+    setAlertVisible(true);
+    setTimeout(() => {
+      setAlertVisible(false);
+    }, 5000);
+  };
+
+  const showSuccessfulCopyAlert = () => {
+    setSuccessfulCopyAlert();
+    showAlert();
+  };
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(linkText);
+  };
+
   return (
     <section className="relative">
       <div className="max-w-6xl mx-auto md:py-4 md:px-4 sm:px-2">
         <div className="mx-4">
           <div
-            className={`bg-red-100 border-l-4 border-red-500 text-red-700 p-4 ${
-              alertVisible ? "visible" : "invisible"
-            }`}
+            className={`${
+              errorAlert
+                ? "bg-red-100 border-l-4 border-red-500 text-red-700"
+                : "bg-green-100 border-l-4 border-green-500 text-green-700"
+            } p-4 ${alertVisible ? "visible" : "invisible"}`}
             role="alert"
           >
-            <p className="font-bold">Invalid Link Provided</p>
-            <p>The provided link is not a valid link. Please check the link and try again.</p>
+            <p className="font-bold">{alertTitle}</p>
+            <p>{alertDesc}</p>
           </div>
           <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-normal mb-24 text-center pt-24">
             <span className="bg-clip-text text-transparent linear-gradient">Link Shortener</span>
@@ -37,14 +88,17 @@ export const Shorten: React.FC<ShortenProps> = ({}) => {
                 className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight md:col-span-10 focus:outline-none focus:shadow-outline md:mr-2 sm:col-span-12"
                 type="text"
                 placeholder="https://www.google.com/"
+                value={linkText}
                 onChange={(e) => {
                   setLinkText(e.currentTarget.value);
                 }}
               />
               <button
-                className="text-gray-300 bg-gray-800 hover:bg-gray-700 hover:text-white font-medium col-span-2 md:col-span-2 text-white py-2 px-4 rounded md:ml-2 sm:col-span-12"
+                className={`text-gray-300 bg-gray-800 hover:bg-gray-700 hover:text-white font-medium col-span-2 md:col-span-2 text-white py-2 px-4 rounded md:ml-2 sm:col-span-12`}
                 onClick={(e) => {
                   e.preventDefault();
+                  buttonToggle ? copyLink() : shortenLink();
+                  buttonToggle && showSuccessfulCopyAlert();
                 }}
               >
                 <span>{buttonToggle ? "Copy" : "Shorten"}</span>
