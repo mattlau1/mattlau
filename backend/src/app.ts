78cdc0b4c482ce2b78d2e3b94ec7742cc241164d
Matthew Lau <matthew.lau78@gmail.com>
2021-09-11 20:40:18 +1000

    fixed cors origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://www.mattlau.codes",
   credentials: true,
   optionSuccessStatus: 200,
 };
