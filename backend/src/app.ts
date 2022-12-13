69e98abef1013ca015d035aacc9130a18d7168c9
Matthew Lau <matthew.lau78@gmail.com>
2022-12-14 01:14:08 +1100

    removed deprecated code; fixed host route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -176,8 +176,7 @@ app.post("/login", async (req, res) => {
   })
 })
 
-app.post("/upload", upload.array('file', 25), async (req, res) => {
-  console.log("/upload hit");
+app.post("/upload", upload.array('file', 32), async (req, res) => {
   if (!req.headers.authorization) return res.send({ status: 400, message: "Missing authorization header" });
   if (!req.files) return res.send({ status: 400, message: "Missing Files" });
   console.log("Processing files: ", req.files);
@@ -235,7 +234,19 @@ app.post("/upload", upload.array('file', 25), async (req, res) => {
 LOCAL ?
   app.listen(process.env.PORT || 5000)
   :
-  module.exports.handler = serverless(app, { binary: ['image/png', 'image/jpeg', 'image/gif', 'video/mp4', 'audio/mpeg'] });
+  module.exports.handler = serverless(app, {
+    binary: [
+      'image/png',
+      'image/jpeg',
+      'image/gif',
+      'video/mp4',
+      'audio/mpeg',
+      'application/pdf',
+      'video/webm',
+      'audio/x-wav',
+      'text/plain'
+    ]
+  });
 
 
 
