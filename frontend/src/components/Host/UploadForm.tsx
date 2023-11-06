91d48d3fa445f8f5439ac492dbd002597c611e2d
Matthew Lau <matthew.lau78@gmail.com>
2023-11-06 19:00:24 +1100

    updated domain redirects

--- frontend/src/components/Host/UploadForm.tsx
+++ frontend/src/components/Host/UploadForm.tsx
@@ -36,7 +36,7 @@ const UploadForm = (props: Props) => {
     axios
       .post(`${process.env.REACT_APP_API || "http://localhost:5000"}/upload`, data, options)
       .then(res => {
-        setUrl("http://zap.mattlau.tech/" + (res.data.urls[0].url.split('/').reverse())[1] + '/' + (res.data.urls[0].url.split('/').reverse())[0]);
+        setUrl("http://zap.mattlau.dev/" + (res.data.urls[0].url.split('/').reverse())[1] + '/' + (res.data.urls[0].url.split('/').reverse())[0]);
         setProgress(100);
       }).catch(e => {
         console.log(e);
